342a6f595880a7cc84c37a3757e2c51be196e975
herzplatinedurchgebrannt <50515453+herzplatinedurchgebrannt@users.noreply.github.com>
2021-01-05 15:42:41 +0100

    Update StepSeq.ino

--- StepSeq/StepSeq.ino
+++ StepSeq/StepSeq.ino
@@ -1,15 +1,36 @@
-void setup() {
- pinMode(4, INPUT);
+byte statusByte;
+byte dataByte1;
+byte dataByte2;
+
+void setup(){
  pinMode(13, OUTPUT); // Eingebaute LED
+ digitalWrite(13, LOW);
+ Serial1.begin(31250);
+
+ digitalWrite(8,HIGH);
 }
 
-void loop() {
- int variable = digitalRead(4);
- if (variable == HIGH) {
+
+void loop(){
+ empfangeMIDI();
+ delay(10);
+ // Ohne Delay leuchtet die LED nicht, dadurch verpasst man aber einige Signale
  digitalWrite(13, LOW);
+}
+
+
+void empfangeMIDI(){
+ do {
+ if (Serial1.available()){
+ 
+ statusByte = Serial1.read();
+ dataByte1 = Serial1.read();
+ dataByte2 = Serial1.read();
+
+ if (statusByte >= 144 && statusByte <= 159) { // Entspricht NoteOn MIDI Kanal 1-16
+ digitalWrite(13,HIGH);
  }
- else {
- digitalWrite(13, HIGH);
- delay(100);
  }
+ }
+ while (Serial1.available() > 2); //Ausführen, wenn mindestens 3 Bytes da sind
 }
